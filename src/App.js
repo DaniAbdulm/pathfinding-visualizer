7bb69e4dc578765b06efad541e34b4b626c7019e
DaniAbdulm <daniabdulm@gmail.com>
2024-07-21 19:18:50 -0400

    created and styled grid

--- src/App.js
+++ src/App.js
@@ -2,29 +2,46 @@ import { useState } from 'react';
 import Node from './components/node';
 import './App.css';
 
-const App = () => {
-  const [grid, setGrid] = useState(createInitialGrid()); //setting up grid using react's useState
-
-  //creating the initial grid
-  const createInitialGrid = () => {
-    const grid = []; 
-    for (let row = 0; row < 20; row++) {
-      const currentRow = []; 
-      for (let col = 0; col < 50; col++) {
-        currentRow.push(createNode(row, col));
-      }
-      grid.push(currentRow);
+// creating the initial grid
+const createInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(row, col));
     }
-    return grid;
+    grid.push(currentRow);
   }
+  return grid;
+};
+
+// creating each box
+const createNode = (row, col) => {
+  return {
+    row,
+    col,
+    isStart: row === 10 && col === 5, // this box is the start if it's at (10, 5)
+    isEnd: row === 10 && col === 45,  // this box is the end if it's at (10, 45)
+    isWall: false, // initially no boxes are walls
+  };
+};
+
+const App = () => {
+  const [grid, setGrid] = useState(createInitialGrid()); // setting up grid using react's useState
 
   return (
     <div className="container">
-      <div className='grid'>
-        
+      <div className="grid">
+        {grid.map((row, rowIdx) => (
+          <div key={rowIdx} className="row">
+            {row.map((node, nodeIdx) => (
+              <Node key={nodeIdx} {...node} />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
